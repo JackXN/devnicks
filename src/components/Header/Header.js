66a11bf2b3b19236a0a39b112813bab9b50b55c5
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-10-03 00:57:50 -0600

    Created particle component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {Box,Text, Flex, Container, Image} from '@chakra-ui/react';
+import Particles from 'react-particles-js';
+import Particle from './Particle';
+
+
+const Logo = '/DLogo.jpeg';
+
+function Header() {
+    return (
+<>
+      <Box style={{background: `url("/HeaderImg.jpeg")`}} sx={styles.headerContainer}>
+          <div id='particles-js'>
+      
+    </div>
+      </Box>
+</>
+   
+    )
+}
+
+
+const styles = {
+    headerContainer: {
+        height:  '50vh',
+        width: '100%',
+        borderRadius:'0 0 50% 50%/0 0 100% 100%',
+        transform:'scaleX(1.5)',
+        position: 'absolute',
+        left: '0',
+        top: '0',
+        display: 'flex',
+    
+    },
+    wrapper: {
+display: 'flex',
+alignItems:'center',
+justifyContent:'center',
+textAlign: 'center',
+    }
+}
+
+export default Header
